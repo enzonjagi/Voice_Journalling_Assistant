1e5118e6b7411cb994578f3f02c0d9646e6e6751
enzonjagi <amosndungo@gmail.com>
2022-03-22 09:59:07 +0300

    Removed a deprecated API

--- fluttercode/voice_journalling/lib/journalling/speech_screen.dart
+++ fluttercode/voice_journalling/lib/journalling/speech_screen.dart
@@ -4,7 +4,6 @@ import 'package:avatar_glow/avatar_glow.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:highlight_text/highlight_text.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 void main() {
